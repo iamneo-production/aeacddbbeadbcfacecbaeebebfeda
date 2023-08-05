c4366e3445b7f5507c083f2972bb223fd12d3038
Iamneo <techadmin@iamneo.ai>
2023-08-05 13:12:21 +0000

    Backup Commit

--- angularapp/src/app/filter.pipe.ts
+++ angularapp/src/app/filter.pipe.ts
@@ -1,12 +1,24 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
 @Pipe({
-  name: 'filter'
-})
-export class FilterPipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  name: "filter"
+  })
+  export class FilterPipe implements PipeTransform {
+  
+  transform(value: any,filter:string){
+  if (filter == ""){
+  return value;
   }
-
-}
+  const foods = [];
+  import { Pipe, PipeTransform } from "@angular/core";
+  
+  for(let v of value)
+  {
+  if(v.name.includes(filter))
+  {
+  foods.push(v);
+  }
+  }
+  
+  return foods;
+  }
+  
+  } 
